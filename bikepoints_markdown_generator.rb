5561287383c12b09e38265671a72838b7aa6ef07
John Boyes <john@edinburghwebservices.co.uk>
2016-11-18 11:50:20 +0000

    Dynamically generate bikepoints markdown file (#9)

---
+++ bikepoints_markdown_generator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require './bikepoint'
+
+def to_markdown_list(array)
+  array.map { |e| ('- ' + e) }
+end
+
+def generate_bikepoints_markdown
+  File.open('BIKEPOINTS.md', 'w+') do |f|
+    f.puts('# All Santander Cycles bikepoint names')
+    f.puts('Copy all the names you wish to receive notifications for into a semi-colon separated
+            list in the `BIKEPOINT_COMMON_NAMES` environment variable.')
+    f.puts(to_markdown_list(Bikepoint.all_names).sort)
+  end
+end
